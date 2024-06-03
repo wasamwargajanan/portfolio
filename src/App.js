8d845b731d3be6d2814de76ada31bed47a72cd8a
Gajanan <gajudattaw@gmail.com>
2024-06-03 18:57:59 +0530

    added new line

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.ok
-        </p>
-       
-      </header>
+      you did it gajju.
     </div>
   );
 }
